cb97f4ed32ab384e38d3c03ea4f6212e06538d09
Zebula Sampedro <sampedro@login03.rc.colorado.edu>
2015-08-03 17:12:54 -0600

    Sets anaconda environment up properly for pyspark.

--- spark-env.sh
+++ spark-env.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
+export _LMFILES_=/curc/tools/Modules/redhat_6_x86_64/supported/slurm/slurm:/curc/tools/Modules/redhat_6_x86_64/supported/jdk/jdk-1.7.0_17:/curc/tools/Modules/redhat_6_x86_64/supported/python/anaconda-2.1.0
+export LOADEDMODULES=slurm/slurm:jdk/jdk-1.7.0_17:python/anaconda-2.1.0
+export PYTHONPATH=/curc/tools/x_86_64/rh6/biopython/1.64/lib/python2.7/site-packages
+
+export PYSPARK_PYTHON=/curc/tools/x_86_64/rh6/anaconda/2.1.0/bin/python
 export SPARK_WORKER_DIR=/lustre/janus_scratch/$USER/spark/work
 export SPARK_LOG_DIR=/lustre/janus_scratch/$USER/spark/logs
 export SPARK_LOCAL_IP=$(sed -e 's/\([^.]*\).*$/\1ib/' <<< $(hostname))
