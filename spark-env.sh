5c596b0390684e47f653e37ddfed6628684c6e7c
Zebula Sampedro <sampedro@login03.rc.colorado.edu>
2015-08-05 18:04:59 -0600

    Properly loads modules on worker nodes.

--- spark-env.sh
+++ spark-env.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+source /etc/profile.d/modules.sh
+
 ml gcc
 ml jdk
 ml python
 ml ALL_PYTHON_PKGS
 
+
 export SPARK_WORKER_DIR=/lustre/janus_scratch/$USER/spark/work
 export SPARK_LOG_DIR=/lustre/janus_scratch/$USER/spark/logs
 export SPARK_LOCAL_IP=$(sed -e 's/\([^.]*\).*$/\1ib/' <<< $(hostname))
+
