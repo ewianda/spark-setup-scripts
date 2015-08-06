bae267b0eee465e7deb197b9753aa5f8d81d4ce2
Zeb Sampedro <sampedro@colorado.edu>
2015-08-06 13:55:28 -0600

    Removing an option that tanked Lustre.

--- spark-env.sh
+++ spark-env.sh
@@ -8,7 +8,6 @@ ml python
 ml ALL_PYTHON_PKGS
 
 
-export SPARK_LOCAL_DIRS=/lustre/janus_scratch/$USER/spark/scratch
 export SPARK_WORKER_DIR=/lustre/janus_scratch/$USER/spark/work
 export SPARK_LOG_DIR=/lustre/janus_scratch/$USER/spark/logs
 export SPARK_LOCAL_IP=$(sed -e 's/\([^.]*\).*$/\1ib/' <<< $(hostname))
